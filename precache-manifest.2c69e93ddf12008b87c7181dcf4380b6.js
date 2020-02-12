0a6b10464bddf8375686a0b7683610daf90beae6
Dioscar Rodriguez <dioscarr@gmail.com>
2020-02-12 11:25:57 -0500

    Updates

---
+++ precache-manifest.2c69e93ddf12008b87c7181dcf4380b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b130f26add52568cdea8e93bd37367",
+    "url": "/moonies/index.html"
+  },
+  {
+    "revision": "a940bca05f4ee01b97ec",
+    "url": "/moonies/static/css/main.14fbc2e9.chunk.css"
+  },
+  {
+    "revision": "3e7d7eabf9d2fec0622f",
+    "url": "/moonies/static/js/2.20107c8c.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/moonies/static/js/2.20107c8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a940bca05f4ee01b97ec",
+    "url": "/moonies/static/js/main.7e8e78b5.chunk.js"
+  },
+  {
+    "revision": "ed398d569814ebc28772",
+    "url": "/moonies/static/js/runtime-main.40ae4a28.js"
+  }
+]);
